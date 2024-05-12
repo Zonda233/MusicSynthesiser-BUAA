4213c6879f4ba348892a1a7bc871c22e2a8307e8
GvdSternen <156628019+GvdSternen@users.noreply.github.com>
2024-05-12 14:15:52 +0800

    Add files via upload
    finish Parser version 0.0

--- MIDIParser.py
+++ MIDIParser.py
@@ -1,7 +1,21 @@
 from typing import Generator
-import numpy as np
 from Note import Note
+from mido import MidiFile
 
 def Parser(filename: str) -> Generator[Note]:
-    '''你的函数注释'''
-    yield Note(0, 0, 0, 0)
+    '''解析 MIDI 文件并生成音符的生成器'''
+    midi = MidiFile(filename)
+    current_time = 0  # 当前时间
+
+    for msg in midi:
+        current_time += msg.time  # 更新当前时间
+
+        if msg.type == 'note_on':
+            # 将音量参数标准化到 0 到 1 之间
+            volume = msg.velocity / 127.0
+            pitch = msg.note
+            time = current_time
+            # 计算时值（duration）和出现时间（time）在 44.1kHz 采样率下的数组长度和数组下标
+            duration = int(msg.time * 44.1)  # 将时间转换为数组长度
+            time_index = int(current_time * 44.1)  # 将时间转换为数组下标
+            yield Note(volume=volume, pitch=pitch, duration=duration, time=time_index)
