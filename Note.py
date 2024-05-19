5a9946a6e056fe2c02e03d7c6fb606ef18280a25
Cloud1-979 <yangpeiye@126.com>
2024-05-19 14:55:49 +0800

    大改Note和Tone的结构，用重载来实现不同音色的不同处理

--- Note.py
+++ Note.py
@@ -26,40 +26,20 @@ class Note:
 
     def wave(self) -> np.array:
         size = self.dura + self.env.R
-        out = np.zeros(size)
-        base = 2 * pi * float(self.pitch) / float(sr)
-        t = np.array(range(size)).astype(np.float64)
-
-        # 根据tone中的各个谐波的振幅、相位信息，生成初始波形
-        for i in range(self.tone.amp.size):
-            amp = self.tone.amp[i]
-            pha = self.tone.pha[i]
-            wave = amp * np.cos((i + 1) * base * t + pha)
-            if self.tone.dec is not None:
-                dec_time = min(self.tone.dec[i], self.dura)
-                env_t = np.linspace(1, 0, dec_time)
-                wave[0:dec_time] *= env_t
-                wave[dec_time:self.dura + self.env.R] = 0
-            out += wave
-        out = out * self.velo
+        out = self.tone.wave(self.velo, size, self.pitch)
 
         env = np.zeros(size)
-        if self.tone.dec is None:
-            if self.dura < self.env.A:
-                env[0:self.dura] = np.linspace(0, 1, self.dura)
-            elif self.dura < self.env.A + self.env.D:
-                env[0:self.env.A] = np.linspace(0, 1, self.env.A)
-                env[self.env.A:self.dura] = np.linspace(1, self.env.S, self.dura - self.env.A)
-            else:
-                env[0:self.env.A] = np.linspace(0, 1, self.env.A)
-                env[self.env.A:self.env.A + self.env.D] = np.linspace(1, self.env.S, self.env.D)
-                env[self.env.A + self.env.D:self.dura] = self.env.S
-
-            env[self.dura:size] = np.linspace(self.env.S, 0, size - self.dura)
+        if self.dura < self.env.A:
+            env[0:self.dura] = np.linspace(0, 1, self.dura)
+        elif self.dura < self.env.A + self.env.D:
+            env[0:self.env.A] = np.linspace(0, 1, self.env.A)
+            env[self.env.A:self.dura] = np.linspace(1, self.env.S, self.dura - self.env.A)
         else:
-            env[0:self.dura] = 1
-            env[self.dura:size] = np.linspace(1, 0, self.env.R)
+            env[0:self.env.A] = np.linspace(0, 1, self.env.A)
+            env[self.env.A:self.env.A + self.env.D] = np.linspace(1, self.env.S, self.env.D)
+            env[self.env.A + self.env.D:self.dura] = self.env.S
 
+        env[self.dura:size] = np.linspace(self.env.S, 0, size - self.dura)
         out *= env
 
         return out
--- Tone.py
+++ Tone.py
@@ -1,13 +1,47 @@
 import numpy as np
 
+from Config import *
+
 class Tone:
-    def __init__(self,
-                 arr_amplitude: np.array,
-                 arr_phase: np.array,
-                 arr_decay_time=None):
-        if arr_amplitude.size != arr_phase.size:
-            raise ValueError("arr_amplitude and arr_phase should have the same size")
-        velo = np.sqrt(np.sum(arr_amplitude ** 2))
-        self.amp = arr_amplitude / velo
-        self.pha = arr_phase.copy()
-        self.dec = arr_decay_time
+    def wave(self, velocity: float, duration: int, pitch: int):
+        """ virtual """
+
+class Tone_Piano(Tone):
+    amp = np.array([0.789, 0.451, 0.058, 0.221, 0.245, 0.064, 0.199, 0.016, 0.101, 0.018, 0.050, 0.017, 0.028, 0.007, 0.039, 0.039, 0.011, 0.012, 0.012, 0.006])
+    pha = np.array([0.806, -0.380, 1.254, -1.752, 2.697, -1.310, -0.032, -1.039, 1.152, -0.854, 2.135, -0.646, 0.482, 0.560, 1.278, 1.278, -0.377, -1.680, -1.680, -3.048])
+    dec = np.array([92600, 90000, 66000, 86000, 57000, 30000, 24000, 24000, 21000, 21000, 21000, 21000, 21000, 21000, 21000, 21000, 21000, 21000, 21000, 21000])
+
+    def __init__(self):
+        """nothing"""
+
+    def wave(self, velocity: float, duration: int, pitch: int):
+        out = np.zeros(duration)
+        t = np.array(range(duration)).astype(np.float64)
+        base = 2 * np.pi * float(pitch) / float(sr)
+        for i in range(self.amp.size):
+            wave = self.amp[i] * np.cos((i + 1) * base * t + self.pha[i])
+            dec_time = min(self.dec[i], duration)
+            env_t = np.linspace(1, 0, dec_time)
+            wave[0:dec_time] *= env_t
+            wave[dec_time:duration] = 0
+            out += wave
+        out = out * velocity
+        return out
+
+tone_piano = Tone_Piano()
+
+class Tone_Violin(Tone):
+    amp = np.array([0.976, 0.159, 0.053, 0.095, 0.061, 0.040, 0.025, 0.061, 0.008, 0.021, 0.007, 0.005, 0.008, 0.003, 0.002, 0.003, 0.002, 0.001, 0.001, 0.001])
+    pha = np.array([-2.668, -1.857, 1.740, -1.102, 3.003, -0.211, -0.053, -0.143, -0.399, -1.253, 2.957, -2.237, -1.754, -1.392, -0.800, -1.351, 0.105, -0.907, -0.398, -1.601])
+
+    def wave(self, velocity: float, duration: int, pitch: int):
+        out = np.zeros(duration)
+        t = np.array(range(duration)).astype(np.float64)
+        base = 2 * np.pi * float(pitch) / float(sr)
+        for i in range(self.amp.size):
+            wave = self.amp[i] * np.cos((i + 1) * base * t + self.pha[i])
+            out += wave
+        out = out * velocity
+        return out
+
+tone_violin = Tone_Violin()
